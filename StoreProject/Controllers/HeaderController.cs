0f1d6fe18ec54c6dee9a708a1e6fe83e4867e36e
TDT <TDT@DESKTOP-M8173L5>
2024-12-16 18:25:06 +0700

    Add project files.

---
+++ StoreProject/Controllers/HeaderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeaderController : ControllerBase
+    {
+        private readonly HeaderRepository _repository;
+
+        public HeaderController(IConfiguration configuration)
+        {
+            _repository = new HeaderRepository(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        [HttpGet("Get")]
+        public IActionResult GetHeaders(int? id = null, Guid? rowPointer = null)
+        {
+            var result = _repository.GetHeaders(id, rowPointer);
+            return Ok(result);
+        }
+
+        [HttpPost("Insert")]
+        public IActionResult InsertHeader([FromBody] HeaderModel model)
+        {
+            _repository.InsertHeader(model);
+            return Ok("Inserted successfully");
+        }
+
+        [HttpPut("Update")]
+        public IActionResult UpdateHeader([FromBody] HeaderModel model)
+        {
+            _repository.UpdateHeader(model);
+            return Ok("Updated successfully");
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult DeleteHeader(int? id = null, Guid? rowPointer = null)
+        {
+            _repository.DeleteHeader(id, rowPointer);
+            return Ok("Deleted successfully");
+        }
+    }
+
+}
---
+++ StoreProject/Repositories/HeaderRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+public class HeaderRepository
+{
+    private readonly string _connectionString;
+
+    public HeaderRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IEnumerable<dynamic> GetHeaders(int? id = null, Guid? rowPointer = null)
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Action", "GET_ALL");
+            parameters.Add("@ID", id);
+            parameters.Add("@RowPointer", rowPointer);
+
+            return db.Query("ManageHeader", parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+    public void InsertHeader(HeaderModel model)
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Action", "INSERT");
+            parameters.Add("@ID", model.ID);
+            parameters.Add("@Column1", model.Column1);
+            parameters.Add("@CreatedBy", model.CreatedBy);
+
+            db.Execute("ManageHeader", parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+    public void UpdateHeader(HeaderModel model)
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Action", "UPDATE");
+            parameters.Add("@ID", model.ID);
+            parameters.Add("@RowPointer", model.RowPointer);
+            parameters.Add("@Column1", model.Column1);
+            parameters.Add("@UpdatedBy", model.UpdatedBy);
+
+            db.Execute("ManageHeader", parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+    public void DeleteHeader(int? id = null, Guid? rowPointer = null)
+    {
+        using (IDbConnection db = new SqlConnection(_connectionString))
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@Action", "DELETE");
+            parameters.Add("@ID", id);
+            parameters.Add("@RowPointer", rowPointer);
+
+            db.Execute("ManageHeader", parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
+
+public class HeaderModel
+{
+    public int? ID { get; set; }
+    public Guid? RowPointer { get; set; }
+    public string Column1 { get; set; }
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+}
